b95f229e063c53feb977d8fb65eb72a6445c65eb
AnjaliGharde <ghardeanjali05@gmail.com>
2025-07-19 08:41:48 -0700

    Update DuplicateStr.java

--- Numbers/DuplicateStr.java
+++ Numbers/DuplicateStr.java
@@ -1,19 +1,37 @@
 package Questions;
 
-public class DuplicateStr {
+public class DuplicateStr{
+    public static void main(String[] args) {
+        String input = "programming";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String str = "hello";
-		int count=0;
-		char a;
-		for(int i=0;i<=str.length()-1;i++) {
-			 a=str.charAt(i);
-			 if(str.charAt(i)==a) {
-				 count++;
-				 System.out.println(str.charAt(i)+ "=" + count);
-			 }
-		}
-	}
+        System.out.println("Duplicate characters:");
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            int count = 0;
 
+     
+            boolean isCounted = false;
+            for (int k = 0; k < i; k++) {
+                if (input.charAt(k) == ch) {
+                    isCounted = true;
+                    break;
+                }
+            }
+
+            if (isCounted)
+                continue;
+
+          
+            for (int j = 0; j < input.length(); j++) {
+                if (input.charAt(j) == ch) {
+                    count++;
+                }
+            }
+
+            if (count > 1) {
+                System.out.println(ch + " → " + count + " times");
+            }
+        }
+    }
 }
