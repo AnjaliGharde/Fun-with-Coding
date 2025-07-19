c1c26fb7ca677ac34a9e4e747dac3dc75731cf61
AnjaliGharde <ghardeanjali05@gmail.com>
2025-07-19 08:32:53 -0700

    Update BounceNum.java

--- Numbers/BounceNum.java
+++ Numbers/BounceNum.java
@@ -1,10 +1,43 @@
 package Questions;
 
 public class BounceNum {
+    public static void main(String[] args) {
+        int number = 12321;  
+        int num = number;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			
-	}
+        int lastDigit = num % 10;
+        num = num / 10;
+        int direction = 0;  
 
+        boolean isBounce = false;
+
+        while (num > 0) {
+            int currentDigit = num % 10;
+
+            if (currentDigit < lastDigit) {
+                if (direction == 0) {
+                    direction = 1;  
+                } else if (direction == -1) {
+                    isBounce = true;
+                    break;
+                }
+            } else if (currentDigit > lastDigit) {
+                if (direction == 0) {
+                    direction = -1;  
+                } else if (direction == 1) {
+                    isBounce = true;
+                    break;
+                }
+            }
+
+            lastDigit = currentDigit;
+            num = num / 10;
+        }
+
+        if (isBounce) {
+            System.out.println(number + " is a Bounce Number.");
+        } else {
+            System.out.println(number + " is NOT a Bounce Number.");
+        }
+    }
 }
